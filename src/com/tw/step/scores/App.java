010755dad3ec0c8a874536f0c2b75cafc3436d0b
Vismaya PS <230529734+Vismayaps2004@users.noreply.github.com>
2026-05-12 22:07:10 +0530

    adds recalculation of whole scores.

--- src/com/tw/step/scores/App.java
+++ src/com/tw/step/scores/App.java
@@ -2,86 +2,74 @@ package com.tw.step.scores;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.time.LocalTime;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 public class App {
     static void main() {
         Path path = Paths.get("resources/incoming");
         Stream<Path> files;
+        int totalScore;
+
         try {
             files = Files.list(path);
+            totalScore = files
+                    .mapToInt(App::readFileScore)
+                    .sum();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        calculateTotalScore(files);
-//        try {
-//            int score = 0;
-//            Scanner scanner = new Scanner(scoresInputFile);
-//            while (scanner.hasNext()) {
-//                score += scanner.nextInt();
-//            }
-//            writeScores(score);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        try {
-//            WatchService watchService = FileSystems.getDefault().newWatchService();
-//            Path path = Paths.get("resources");
-//            path.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-//
-//            WatchKey key;
-//            for (; ; ) {
-//                try {
-//                    key = watchService.take();
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                for (WatchEvent<?> event: key.pollEvents()) {
-//                    Path fileName = (Path) event.context();
-//                try {
-//                    int score = 0;
-//                    Scanner scanner = new Scanner(fileName);
-//                    while (scanner.hasNext()) {
-//                        score += scanner.nextInt();
-//                    }
-//                    writeScores(score);
-//                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-//                }
-//
-//                }
-//                boolean valid = key.reset();
-//                if (!valid) {
-//                    break;
-//                }
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-    }
+        writeScores(totalScore);
+        try {
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+            path.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
 
-    private static void calculateTotalScore(Stream<Path> files) {
-        AtomicInteger score = new AtomicInteger();
-        files.forEach(file -> {
-            try {
-                Scanner scanner = new Scanner(file);
-                while (scanner.hasNext()) {
-                    score.addAndGet(scanner.nextInt());
+            WatchKey key;
+            for (; ; ) {
+                try {
+                    key = watchService.take();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    Thread.sleep(100);
+                    try (Stream<Path> updatedFiles = Files.list(path)) {
+                        totalScore = updatedFiles
+                                .mapToInt(App::readFileScore)
+                                .sum();
+                        writeScores(totalScore);
+                    }
+                }
+                boolean valid = key.reset();
+                if (!valid) {
+                    break;
+                }
             }
-        });
-        writeScores(score.get());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static void writeScores(Integer scoreDetails) {
+    private static int readFileScore(Path file) {
+        int score = 0;
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextInt()) {
+                score += scanner.nextInt();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return score;
+    }
+
+    private static void writeScores(int scoreDetails) {
         try (FileWriter writer = new FileWriter("resources/scores.txt", true)) {
 
             LocalTime time = LocalTime.now();
