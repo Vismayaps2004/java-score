c9b732fcf43927dfc5a14bb742298e5d9bce605c
Vismaya PS <230529734+Vismayaps2004@users.noreply.github.com>
2026-05-12 18:32:45 +0530

    Adds resoiurce folder in watcher

--- src/com/tw/step/scores/App.java
+++ src/com/tw/step/scores/App.java
@@ -1,18 +1,40 @@
 package com.tw.step.scores;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.Scanner;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 public class App {
     static void main() {
-        int score = 0;
-        Path scoresInputFile = Paths.get("resources/scores-file1.txt");
         try {
-            Scanner scanner = new Scanner(scoresInputFile);
-            while (scanner.hasNext()) {
-                score += scanner.nextInt();
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+            Path path = Paths.get("resources");
+            path.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+
+            WatchKey key;
+            for (; ; ) {
+                try {
+                    key = watchService.take();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                int score = 0;
+                Path scoresInputFile = Paths.get("resources/scores-file1.txt");
+                try {
+                    Scanner scanner = new Scanner(scoresInputFile);
+                    while (scanner.hasNext()) {
+                        score += scanner.nextInt();
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(score);
+                boolean valid = key.reset();
+                if (!valid) {
+                    break;
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
