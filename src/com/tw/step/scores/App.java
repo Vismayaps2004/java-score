a25e72bc018df3243c51aae97b3578ecda8be610
Vismaya PS <230529734+Vismayaps2004@users.noreply.github.com>
2026-05-12 17:47:24 +0530

    added scores in file

--- src/com/tw/step/scores/App.java
+++ src/com/tw/step/scores/App.java
@@ -1,23 +1,19 @@
 package com.tw.step.scores;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class App {
     static void main() {
+        int score = 0;
         Path scoresInputFile = Paths.get("resources/scores-file1.txt");
         try {
             Scanner scanner = new Scanner(scoresInputFile);
-            Integer score;
-            while(scanner.hasNext()) {
-                System.out.println(scanner.nextInt());
-//                score += scanner.nextInt();
+            while (scanner.hasNext()) {
+                score += scanner.nextInt();
             }
-            System.out.println(scanner);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
